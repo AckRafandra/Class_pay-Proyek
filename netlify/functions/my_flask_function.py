00ad3535450bc2ca792258e5c1f7126a72ccd949
Ackerman Rafanda <ackermanrafandra@gmail.com>
2024-12-08 22:31:35 +0700

    Update my_flask_function.py

--- netlify/functions/my_flask_function.py
+++ netlify/functions/my_flask_function.py
@@ -9,23 +9,38 @@ model = joblib.load('random_forest_model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()  # Ambil data JSON dari frontend
-    features = np.array([[  # Transform data jadi input model
-        data['age'],
-        data['quantity'],
-        data['price_per_unit'],
-        data['quantity'] * data['price_per_unit'],
-        1 if data['gender'] == 'Male' else 0,
-        1 if data['product_category'] == 'Clothing' else 0,
-        1 if data['product_category'] == 'Electronics' else 0,
-        1 if data['city'] == 'Denpasar' else 0,
-        1 if data['city'] == 'Jakarta' else 0,
-        1 if data['city'] == 'Surabaya' else 0
+    data = request.get_json()
+    gender = data['gender']
+    age = int(data['age'])
+    product_category = data['product_category']
+    city = data['city']
+    quantity = int(data['quantity'])
+    price_per_unit = float(data['price_per_unit'])
+
+    # Prediksi seperti biasa
+    features = np.array([[  # Masukkan logika Anda di sini
+        age,
+        quantity,
+        price_per_unit,
+        quantity * price_per_unit,
+        1 if gender == 'Male' else 0,
+        1 if product_category == 'Clothing' else 0,
+        1 if product_category == 'Electronics' else 0,
+        1 if city == 'Denpasar' else 0,
+        1 if city == 'Jakarta' else 0,
+        1 if city == 'Makassar' else 0,
+        1 if city == 'Medan' else 0,
+        1 if city == 'Semarang' else 0,
+        1 if city == 'Surabaya' else 0,
+        1 if city == 'Yogyakarta' else 0
     ]])
 
     prediction = model.predict(features)[0]
     payment_methods = {0: 'Cash', 1: 'E-wallet', 2: 'Credit Card'}
-    return jsonify({'prediction_text': f'Metode Pembayaran: {payment_methods[prediction]}'})
+    result = payment_methods[prediction]
+
+    return jsonify({"prediction_text": f"Metode Pembayaran: {result}"})
+
 
 # Convert Flask app ke Netlify Function handler
 from flask_lambda import FlaskLambda
